fdfd5d0abf61eb5485dff29610aeb7b187ea113a
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2022-12-12 18:50:16 +0530

    more stars Q

--- lec2.java
+++ lec2.java
@@ -768,14 +768,37 @@
 // 1234
 // 12345
 
+// public class lec2 {
+//     public static void main(String args[]) {
+//         int n = 5;
+//         for (int i = 1; i <= n; i++) {
+//             for(int j=1;j<=i ; j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Q.8 
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
 public class lec2 {
     public static void main(String args[]) {
         int n = 5;
+        int number = 1;
+
         for (int i = 1; i <= n; i++) {
-            for(int j=1;j<=i ; j++){
-                System.out.print(j);
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number);
+                number++;
             }
             System.out.println();
+
         }
     }
 }

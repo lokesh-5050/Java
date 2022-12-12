8aa7ca76be33549674c1a8db443657dca88ed314
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2022-12-12 22:58:48 +0530

    806

--- lec2.java
+++ lec2.java
@@ -780,25 +780,53 @@
 //     }
 // }
 
-// Q.8 
+// Q.8 -floyd-triangle
 // 1
 // 2 3
 // 4 5 6
 // 7 8 9 10
 // 11 12 13 14 15
 
+// public class lec2 {
+//     public static void main(String args[]) {
+//         int n = 5;
+//         int number = 1;
+
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print(number);
+//                 number++;
+//             }
+//             System.out.println();
+
+//         }
+//     }
+// }
+
+// Q. 0-1 triangle
+// 1
+// 01
+// 101
+// 0101
+// 10101
+
 public class lec2 {
     public static void main(String args[]) {
         int n = 5;
-        int number = 1;
+        // int number = 1;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(number);
-                number++;
+                int sum = i+j;
+                if(sum % 2 == 0){
+                    System.out.print(1);
+                    
+                }else{
+                    System.out.print(0);
+                }
             }
             System.out.println();
-
         }
+
     }
 }

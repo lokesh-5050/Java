8867f04df7da8422ee2463036a53ca749b959b28
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2022-12-13 12:05:04 +0530

    1024 . Palindromic Pattern

--- lec2.java
+++ lec2.java
@@ -1019,3 +1019,29 @@
 //         }
 //     }
 // }
+
+
+// Q. Palindromic Pattern
+        
+        //     1
+        //    212
+        //   32123
+        //  4321234
+        // 543212345
+
+
+public class lec2{
+    public static void main(String args[]) {
+        int r= 5;
+
+        for(int i=1 ; i<=r ; i++){
+            //1st- spaces
+            for(int j =1 ; i<=r-i ; j++){
+                System.out.print(" ");
+            }
+
+            //2nd- palindrome
+            for(int j = 1 ; j<=)
+        }
+    }
+}

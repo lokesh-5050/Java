3ce290b8fde2393c0818a8c1391969d06990f052
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2022-12-27 14:35:58 +0530

    2d array q

--- Arrays/twoDArrays.java
+++ Arrays/twoDArrays.java
@@ -73,32 +73,77 @@ import java.util.*;
 
 public class twoDArrays {
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int r = sc.nextInt();
-        int c = sc.nextInt();
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int r = sc.nextInt();
+    int c = sc.nextInt();
 
-        int number[][] = new int[r][c];
-        //inserting values at [i][j]
-        for(int i = 0;i<r ;i++){
-            for(int j = 0;j<c ;j++){
-                number[i][j] = sc.nextInt();
-            }
-            System.out.println();
-        }
-        //printing values at [i][j]
-        for(int i=0 ; i<r ;i++){
-            for(int j = 0 ;j<c ;j++){
-                System.out.print(number[i][j]+ " ");
-            }
-            System.out.println();
-        }
-
-        //now we have to print all  the valeus of array as numbers
-//         1  5  7  9 10 11      
- //        6 10 12 13 20 21   
-//         9 25 29 30 32 41   
-//        15 55 59 63 68 70  
-//        40 70 79 81 95 105 
+    int number[][] = new int[r][c];
+    //inserting values at [i][j]
+    for (int i = 0; i < r; i++) {
+      for (int j = 0; j < c; j++) {
+        number[i][j] = sc.nextInt();
+      }
+      System.out.println();
     }
+    //printing values at [i][j]
+    for (int i = 0; i < r; i++) {
+      for (int j = 0; j < c; j++) {
+        System.out.print(number[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+    //now we have to print all  the valeus of array as numbers
+    //         1  5  7  9 10 11
+    //        6 10 12 13 20 21
+    //         9 25 29 30 32 41
+    //        15 55 59 63 68 70
+    //        40 70 79 81 95 105
+
+    // for (int i = 0; i < r; i++) {
+    //   for (int j = 0; j < c; j++) {
+    //     System.out.print(number[i][j] + " ");
+    //   }
+    //   System.out.print("");
+    // }
+
+    int last_row = r - 1;
+    int last_col = c - 1;
+
+    //1
+    for(int i=0;i<r-last_row ; i++){
+        for(int j = 0 ; j<c ;j++){
+            System.out.print(number[i][j]+" ");
+        }
+        System.out.print(" ");
+    }
+    
+    //2nd
+    for(int i = 1 ;i<r ; i++){
+        for(int j = last_col ; j<c ; j++ ){
+            System.out.print(number[i][j]+ " ");
+        }
+        System.out.print(" ");
+    }
+
+    //3rd
+    for(int i = last_row ; i<r ; i++){
+        for(int j = last_col-1 ; j>=0 ; j-- ){
+            System.out.print(number[i][j]+ " ");
+        }
+        System.out.print(" ");
+    }
+
+    //4th
+    for(int i = last_row-1 ; i>0 ; i--){
+        for(int j = c-last_col ; j<=c-last_col ; j++){
+            System.out.print(number[i][j]+ " ");
+        }
+        System.out.print(" ");
+    }
+
+
+
+  }
 }
